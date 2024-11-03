a703d393c02e8e996e5485985482dcdb56daf1ab
Luiz Carlos <luizzlcs@gmail.com>
2024-11-03 16:04:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
+	
+	static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Bocks");
 		
-		System.out.println(obj);
+		Department obj = new Department(1, "Bocks");
+		Seller seller = new Seller(1,"Luiz", "luizzlcs@gmail.com",  LocalDate.parse("20/02/1984", dtf), 1200.00, obj); 
+		
+		System.out.println(seller);
 
 	}
 
