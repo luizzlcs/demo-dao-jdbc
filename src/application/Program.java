7122b90961efa316c986f44011fee1a9baa6a0f1
Luiz Carlos <luizzlcs@gmail.com>
2024-11-04 15:28:54 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,7 +30,7 @@ public class Program {
 			System.out.println(obj);
 			
 		}
-		System.out.println("\n=== TEST 3: Seller findAll ===");
+		System.out.println("\n=== TEST 4: Seller findAll ===");
 		list = sellerDao.findAll();
 		
 //		list.forEach(System.out::print)	
@@ -37,6 +38,14 @@ public class Program {
 			System.out.println(obj);
 			
 		}
+		
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		
 	}
 
 }
