580e6f4eeb6f1fe9d5a6893600724805038310c9
Luiz Carlos <luizzlcs@gmail.com>
2024-11-04 12:19:19 -0300

    indByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -14,11 +17,20 @@ public class Program {
 		
 		System.out.println("=== TEST 1: Seller findById ===");
 		Seller seller = sellerDao.findByid(1);
-		 
-		
 		
 		System.out.println(seller);
 
+		System.out.println("\n=== TEST 2: Seller findByDepartment ===");
+		
+		Department department = new Department(2, null);
+		
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+//		list.forEach(System.out::print);
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+		}
 	}
 
 }
