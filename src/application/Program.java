866a4d8f256225f70814ccc8649fcbaad288bfd9
Luiz Carlos <luizzlcs@gmail.com>
2024-11-05 12:09:26 -0300

    DepartmentDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,41 +16,41 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-//		System.out.println("=== TEST 1: Seller findById ===");
-//		Seller seller = sellerDao.findByid(1);
-//		
-//		System.out.println(seller);
-//
-//		System.out.println("\n=== TEST 2: Seller findByDepartment ===");
-//		Department department = new Department(2, null);
-//		List<Seller> list = sellerDao.findByDepartment(department);
-//		
-////		list.forEach(System.out::print);
-//		for (Seller obj : list) {
-//			System.out.println(obj);
-//			
-//		}
-//		System.out.println("\n=== TEST 4: Seller findAll ===");
-//		list = sellerDao.findAll();
-//		
-////		list.forEach(System.out::print)	
-//		for (Seller obj : list) {
-//			System.out.println(obj);
-//			
-//		}
+		System.out.println("=== TEST 1: Seller findById ===");
+		Seller seller = sellerDao.findByid(1);
+		
+		System.out.println(seller);
+
+		System.out.println("\n=== TEST 2: Seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+//		list.forEach(System.out::print);
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+		}
+		System.out.println("\n=== TEST 4: Seller findAll ===");
+		list = sellerDao.findAll();
+		
+//		list.forEach(System.out::print)	
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+		}
 		
 		
-//		System.out.println("\n=== TEST 4: seller insert =====");
-//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-//		sellerDao.insert(newSeller);
-//		System.out.println("Inserted! New id = " + newSeller.getId());
-//		
-//		System.out.println("\n=== TEST 5: seller Update =====");
-//		Seller find = sellerDao.findByid(1);
-//		find.setName("Maria");
-//		find.setBaseSalary(4890.45);
-//		sellerDao.update(find);
-//		System.out.println("UpdanteInserted! in id = " + find.getId());
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller Update =====");
+		Seller find = sellerDao.findByid(1);
+		find.setName("Maria");
+		find.setBaseSalary(4890.45);
+		sellerDao.update(find);
+		System.out.println("UpdanteInserted! in id = " + find.getId());
 		
 		System.out.println("\n=== TEST 6: seller Delete =====");
 		int idSeller = 8;
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,47 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.util.List;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+	
+	Connection conn = null;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findByid(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
